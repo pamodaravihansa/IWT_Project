5d0491ede9f0faa7a65ed7a86fc70925b0044217
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-05-10 16:30:09 +0530

    complete project

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+function clr(){
+    document.getElementById("pname").value ="";
+    document.getElementById("phone").value ="";
+    document.getElementById("specinput").value ="";
+    document.getElementById("docinput").value ="";
+    document.getElementById("date").value ="";
+}
+
+
+function clr2(){
+    document.getElementById("nameoncard").value ="";
+    document.getElementById("cardnum").value ="";
+    document.getElementById("month").value ="";
+    document.getElementById("seccode").value ="";    
+}
+
+
+
